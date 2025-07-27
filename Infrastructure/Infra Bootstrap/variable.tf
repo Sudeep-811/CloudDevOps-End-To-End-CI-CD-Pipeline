a2d8e6de63e5d1d1d6c55e9dadb61aa1044b5847
Sudeep-811 <Sudeepkv811@gmail.com>
2025-07-28 02:23:08 +0530

    infra and deploy10

--- Infrastructure/Infra Bootstrap/variable.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_region"       { default = "ap-south-1" }
-variable "state_bucket_name"{ default = "my-tf-state-bucket-rex-2025" }
-variable "lock_table_name"  { default = "my-tf-lock-table-rex-2025" }
-variable "app_name"         { default = "web-app" }
