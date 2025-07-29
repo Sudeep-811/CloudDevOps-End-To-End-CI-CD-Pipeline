6347d24bd47422fd725c89c2e6be8c01ee8c5b54
Sudeep-811 <Sudeepkv811@gmail.com>
2025-07-29 20:57:20 +0530

    infra and deploy1.6

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -70,10 +70,11 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_a.id
+  subnet_id = values(aws_subnet.public_subnets)[0].id
   tags = {
     Name = "${var.app_name}-nat-gw"
   }
+  depends_on = [aws_internet_gateway.main_igw]
 }
 
 resource "aws_route_table" "private_rt" {
