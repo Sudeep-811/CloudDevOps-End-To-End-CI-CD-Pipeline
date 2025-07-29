b6ca45c9f040d21bb542dc7eb84839b007f99787
Sudeep-811 <Sudeepkv811@gmail.com>
2025-07-29 20:49:58 +0530

    infra and deploy1.4

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -62,6 +62,40 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnets[each.key].id
   route_table_id = aws_route_table.public_rt.id
 }
+# --- NAT and private connection ---
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_a.id
+  tags = {
+    Name = "${var.app_name}-nat-gw"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "${var.app_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  for_each = var.private_subnets
+
+  subnet_id      = aws_subnet.private_subnets[each.key].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
 
 # --- ECS Cluster & ECR ---
 
